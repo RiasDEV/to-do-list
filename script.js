ad91bfbd6992d9c6fa8249973165db1e5e496601
RiasDEV <128004871+RiasDEV@users.noreply.github.com>
2023-10-03 17:31:42 -0300

    att4 js

---
+++ script.js
@@ -0,0 +1,17 @@
+function newTask()
+{
+    let input = document.getElementById('input-new-task')
+
+    // validation
+    if(!input.value)
+    {
+        alert('Type something to add, to your tasks')
+    }
+    // else if()
+    else
+    {
+        // increment to localStorage
+        let values = JSON.parse()
+
+    }
+}
